b72c5d0f061481b9b00b9f88c6d73d1d734802de
Halesh R <01fe22bca124@kletech.ac.in>
2024-02-07 16:46:42 +0530

    Code for Calculator with Multi Functions

---
+++ ExtCal.java
@@ -0,0 +1,20 @@
+ public class ExtCal {
+       
+    public float square (float num1)
+        {
+           return(num1*num1);
+        }
+    
+        public float cube (float num1)
+        {
+           return(num1*num1*num1);
+        }
+    
+        public float modulus (float num1,float num2)
+        {
+           return(num1 % num2);
+        }
+    
+    
+}
+
